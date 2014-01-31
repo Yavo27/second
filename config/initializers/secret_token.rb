555432a532064df362a81efa6c74c27759b4b6e5
Yon Vo <yonavo@gmail.com>
2014-01-31 23:14:02 +0700

    Initial commit this is coolios

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Second::Application.config.secret_key_base = '5ff059b35dc0ceebf7c0987e229bdf589370db6b0a7e44c42637bbb0e661a8154fd46e42888088929969bc2349815580d52d319eca83351e1d28b05bd4ff1dce'
